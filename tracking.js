e0d87700279175576418cfee12942b70255f9393
The Mindless Robot <joelarias@protonmail.com>
2020-06-25 12:38:28 -0700

    builds params correctly with fallbacks

--- tracking.js
+++ tracking.js
@@ -8,26 +8,34 @@ for(const link of trackingLinks) {
     console.dir(link);
     link.addEventListener('click', (e)=>{
         const trackingParams = getParams(e);
+        sendEvent(trackingParams);
     });
 
 
 }
 
+function sendEvent(params = false, action = false) {
+    const eAction = action !== false ? action : 'click';
+    console.log('sending event', params);
+}
 
+// TODO: if label is empty replace with link text
 function getParams(e) {
     const params = {};
-    console.log(e);
     const dataset = e.target.dataset;
-    console.log(dataset);
+
     for(const data in dataset) {
         let key = data;
+        const value = dataset[data];
+
         if(data == 'label')
             key = 'event_label';
         if(data == 'category')
             key = 'event_category';
 
-        params[key] = dataset[data];
+        if(typeof value == 'string' && value.length > 0)
+            params[key] = value;
     }
-    console.log(params);
+
     return params;
 }
