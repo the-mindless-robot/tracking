3c1e96cc2f2613dc3d5bdd3507dd25e9478f9834
The Mindless Robot <joelarias@protonmail.com>
2020-06-25 12:16:14 -0700

    get params

--- tracking.js
+++ tracking.js
@@ -1,5 +1,33 @@
+// generic tracking function
+// based on:
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+
 const trackingLinks = document.getElementsByClassName('track');
 
 for(const link of trackingLinks) {
     console.dir(link);
+    link.addEventListener('click', (e)=>{
+        const trackingParams = getParams(e);
+    });
+
+
+}
+
+
+function getParams(e) {
+    const params = {};
+    console.log(e);
+    const dataset = e.target.dataset;
+    console.log(dataset);
+    for(const data in dataset) {
+        let key = data;
+        if(data == 'label')
+            key = 'event_label';
+        if(data == 'category')
+            key = 'event_category';
+
+        params[key] = dataset[data];
+    }
+    console.log(params);
+    return params;
 }
